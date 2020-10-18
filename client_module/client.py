a9920cae67f4bacc4b7239042dcf6246778deb98
Kirill Kovalev <visclaus2025@gmail.com>
2020-10-18 21:02:10 +0400

    refactor

--- client_module/client.py
+++ client_module/client.py
@@ -2,7 +2,33 @@ import socket
 import ssl
 
 if __name__ == '__main__':
-    data = "Hello".encode()
-    serv = ssl.wrap_socket(socket.socket())
-    serv.connect(('server_url', 443))
-    serv.send(data)
+
+    host_ip = '127.0.0.1'
+    port = 1234
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(True)
+    sock.connect((host_ip, port))
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations('server.crt')
+    context.load_cert_chain(certfile="client.crt", keyfile="client.key")
+
+    if ssl.HAS_SNI:
+        secure_sock = context.wrap_socket(sock, server_side=False, server_hostname=host_ip)
+    else:
+        secure_sock = context.wrap_socket(sock, server_side=False)
+
+    if not secure_sock.getpeercert(): raise Exception("No server's certificate!")
+    print("Sending image")
+    with open('to_send.jpg', 'rb') as f:
+        bytes_pack = f.read(1024)
+        number = 0
+        while bytes_pack:
+            print("Sending #{0} package of 1024 bytes".format(number))
+            secure_sock.send(bytes_pack)
+            bytes_pack = f.read(1024)
+        print("Sending successful!")
+    secure_sock.close()
+    sock.close()
--- server_module/server.py
+++ server_module/server.py
@@ -1,16 +1,37 @@
 import socket
 import ssl
 
-
-class Server(object):
-
-    def __init__(self, port):
-        self.port = port
-
-
 if __name__ == '__main__':
-    sock = ssl.wrap_socket(socket.socket(), 'server.key', 'server.crt', True)
-    sock.bind(('localhost', 43433))
-    sock.listen(10)
-    conn, addr = sock.accept()
-    data = conn.recv(1024)
+
+    # print(repr(secure_sock.getpeername()))
+    # print(secure_sock.cipher())
+    # print(cert)
+
+    host_ip = '127.0.0.1'
+    port = 1234
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host_ip, port))
+    server_socket.listen(10)
+
+    client, fromaddr = server_socket.accept()
+    secure_sock = ssl.wrap_socket(client, server_side=True, ca_certs="client.crt", certfile="server.crt",
+                                  keyfile="server.key", cert_reqs=ssl.CERT_REQUIRED,
+                                  ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+    # verify client
+    if not secure_sock.getpeercert(): raise Exception("No client's certificate!")
+    with open("received.jpg", 'wb') as f:
+        while True:
+            print("Receiving image!")
+            bytes_pack = secure_sock.recv(1024)
+            number = 1
+            while bytes_pack:
+                print("Receiving #{0} package of 1024 bytes".format(number))
+                f.write(bytes_pack)
+                bytes_pack = secure_sock.recv(1024)
+                number += 1
+            print("Receiving successful!")
+            secure_sock.close()
+            server_socket.close()
